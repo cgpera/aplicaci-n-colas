9284452061057cadc8cc6feb530b401a8eabe3cb
Claudio Pera <cgpera@gmail.com>
2020-04-01 16:15:51 -0300

    Primer commit - Producción

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,28 @@
+// Comando para establecer la conexión
+
+var socket = io();
+
+var label = $('#lblNuevoTicket');
+
+socket.on('connect', () => {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', () => {
+    console.log('Desconectado del servidor');
+});
+
+socket.on('estadoActual', (resp) => {
+    label.text(resp.actual);
+    console.log(resp);
+})
+
+$('button').on('click', function() {
+
+    socket.emit('siguienteTicket', null, function(siguienteTicket) {
+
+        label.text(siguienteTicket);
+
+    });
+
+});
---
+++ public/js/socket.publico.js
@@ -0,0 +1,34 @@
+// Comando para establecer la conexión
+
+var socket = io();
+
+socket.on('connect', () => {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', () => {
+    console.log('Desconectado del servidor');
+});
+
+socket.on('estadoActual', function(data) {
+    actualizaHTML(data);
+})
+
+socket.on('ultimos4', function(data) {
+    //    actualizaHTML(data);
+    var audio = new Audio('./audio/new-ticket.mp3')
+    audio.play();
+    data.ultimos4.forEach((element, idx) => {
+        $(`#lblTicket${idx + 1}`).text(`Ticket ${element.numero}`);
+        $(`#lblEscritorio${idx + 1}`).text(`Escritorio ${element.escritorio}`);
+    });
+})
+
+function actualizaHTML(array) {
+    array.ultimos4.forEach((element, idx) => {
+        $(`#lblTicket${idx + 1}`).text(`Ticket ${element.numero}`);
+        $(`#lblEscritorio${idx + 1}`).text(`Escritorio ${element.escritorio}`);
+        console.log(element);
+    });
+
+}
